548ead93a09ed24cdaabe78415f91469f9bed75a
shefalisachdeva <156178715+shefalisachdeva@users.noreply.github.com>
2025-01-22 09:29:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+
+// console.log(typeof(express));
+const app=express();
+// console.log(app);
+
+app.get("/",(req,res)=>{    
+res.send("hello frontend");
+});
+
+app.get("/about",(req,res)=>{    
+    res.send("hello about page");
+    });
+
+app.get("/contact",(req,res)=>{    
+    res.send("hello contact page");
+    });
+app.listen(3000,()=>{    
+    console.log("Server is running at port 3000");
+}); 
